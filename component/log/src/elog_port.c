39a801d6f458d019a7550a848558ff046a73f254
aliyun8725643659 <kunkun_deng@163.com>
2025-07-27 19:34:31 +0800

    refactor: Simplify HAL inclusion and improve CMake configurations for log, RCC, and shell components

--- component/log/src/elog_port.c
+++ component/log/src/elog_port.c
@@ -29,19 +29,7 @@
 #include <elog.h>
 #include <stdio.h>
 #include "cmsis_os2.h"
-
-#if defined(STM32F4) || defined(STM32F411xE)
-#pragma message("use STM32F4 family HAL")
-#include "stm32f4xx_hal.h"
-#elif defined(STM32H7) || defined(STM32H7xx) || defined(STM32H743xx) || defined(STM32H753xx)
-#pragma message("use STM32H7 family HAL")
-#include "stm32h7xx_hal.h"
-#elif defined(STM32F1) || defined(STM32F103xE)
-#pragma message("use STM32F1 family HAL")
-#include "stm32f1xx_hal.h"
-#else
-#error "Unknown STM32 family, HAL header not included"
-#endif
+#include "hal.h"
 extern UART_HandleTypeDef huart1;
 
 static void elog_entry(void *para);
